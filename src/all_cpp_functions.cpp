4d3710b226f11e75afd8f7769f5c069c3d67c511
isgauper <isgauper@gmail.com>
2025-10-16 10:40:28 +1300

    corrected pi_i2 name

--- src/all_cpp_functions.cpp
+++ src/all_cpp_functions.cpp
@@ -10,18 +10,18 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 IntegerMatrix perm2(IntegerVector v) {
-  
+
   // get length of the sequence
   int n = v.size();
-  
+
   // get n! for total number of permutations
   int total = 1;
   for (int i = 2; i <= n; i++)
     total = total * i;
-  
+
   // create matrix for X
   IntegerMatrix X(total, n);
-  
+
   // use next_permutation to get all permutations
   int row = 0;
   do {
@@ -30,7 +30,7 @@ IntegerMatrix perm2(IntegerVector v) {
     }
     row++;
   } while (std::next_permutation(v.begin(), v.end()));
-  
+
   return X;
 }
 
@@ -39,15 +39,15 @@ IntegerMatrix perm2(IntegerVector v) {
 // Gets the row number of a sequence of numbers from the list of all permutations
 
 // [[Rcpp::export]]
-int pi_i2_v2(IntegerVector pat) {
+int pi_i2(IntegerVector pat) {
   int a = pat.size() - 1;
-  
+
   // get sequence from 0 to a
   IntegerVector seq_vec(a + 1);
   for (int i = 0; i <= a; i++) {
     seq_vec[i] = i;
   }
-  
+
   // get all permutations using perm2
   IntegerMatrix op = perm2(seq_vec);
 
@@ -57,7 +57,7 @@ int pi_i2_v2(IntegerVector pat) {
     if (is_true(all(row == pat)))  // check if the row matches the pattern
       return i + 1;       // R indexing starts at 1. C++ starts at 0
   }
-  
+
 
 }
 
